489bd40c122272903843320bb051437eb4b3d1cd
sesughyaagba <sesughdesmond@gmail.com>
2023-02-08 12:38:35 +0100

    first commit

---
+++ Iteration.js
@@ -0,0 +1,18 @@
+for (let i=0; i<=15; i++) {
+    
+    if (i === 0)  {
+        console.log(i + ' is even');
+    }
+
+    else if (i%2 === 0) { 
+        console.log(i + ' is even')
+    }
+
+    else {
+        console.log(i + ' is odd');
+    }
+}
+
+
+for (let x=0; x>5; x--)
+console.log(x)
